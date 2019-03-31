3ffadab228d0a1bb144a0ffad0942d8e3dd12825
Matt Hagy <matthew.hagy@gmail.com>
2019-03-31 08:42:30 -0700

    added temporary script to publish to matthagy.com

---
+++ scripts/publish.sh
@@ -0,0 +1,8 @@
+acp() {
+	aws s3 cp $* &
+}
+
+acp target/scala-2.12/chem_prog_exp-opt.js s3://matthagy-com/target/scala-2.12/chem_prog_exp-opt.js
+acp random_walk_scalajs.html s3://matthagy-com/random_walk.html
+
+wait
