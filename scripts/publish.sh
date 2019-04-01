6992f25eee591673a09a1db7731db2b04148285e
Matt Hagy <matthew.hagy@gmail.com>
2019-04-01 06:44:23 -0700

    revised soft spheres

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,5 +4,7 @@ acp() {
 
 acp target/scala-2.12/chem_prog_exp-opt.js s3://matthagy-com/target/scala-2.12/chem_prog_exp-opt.js
 acp random_walk_scalajs.html s3://matthagy-com/random_walk.html
+acp soft_spheres.html s3://matthagy-com/soft_spheres.html
+
 
 wait
