2570b9d8bd8d737b460d8faf940eb318399dc162
Matt Hagy <matthew.hagy@gmail.com>
2019-05-19 11:40:17 -0700

    publish ising

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,6 +6,7 @@ acp target/scala-2.12/chem_prog_exp-opt.js s3://matthagy-com/target/scala-2.12/c
 acp random_walk_scalajs.html s3://matthagy-com/random_walk.html
 acp soft_spheres.html s3://matthagy-com/soft_spheres.html
 acp polymers.html s3://matthagy-com/polymers.html
+acp ising.html s3://matthagy-com/ising.html
 
 
 wait
